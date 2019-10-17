3784b917f22e7d78d31110d471563882530ed29f
nadianadeem <55580386+nadianadeem@users.noreply.github.com>
2019-10-17 23:47:11 +0100

    V3
    Added obstacles into the a folder for later use. I made the ship face the direction its moving when using the left and right arrow keys, I found out by using the Unity documentation.

--- 2DBoatRacingNN/Assets/Scripts/movement.cs
+++ 2DBoatRacingNN/Assets/Scripts/movement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+    // These are KeyCodes are set so in Unity I can see the keys I want to use.
     public KeyCode upKey;
     public KeyCode downKey;
     public KeyCode leftKey;
@@ -11,15 +12,20 @@ public class movement : MonoBehaviour
 
     public float moveSpeed = 5f;
 
+    //Variable created so I can manipulate the SpriteRenderer using RedSpriteRenderer.
+    private SpriteRenderer RedSpriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Once the game starts the variable has an attachment to the SpriteRenderer Component of the red pirate ship.
+        RedSpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        GetComponent<Rigidbody2D>().velocity = Vector2.left * 1;
         if (Input.GetKey(upKey))
         {
             GetComponent<Rigidbody2D>().velocity = Vector2.up * moveSpeed;
@@ -30,10 +36,14 @@ public class movement : MonoBehaviour
         }
         if (Input.GetKey(leftKey))
         {
+            //The sprite is flipped using the 'flipX' method from the SpriteRenderer so it points the other direction.
+            RedSpriteRenderer.flipX = true;
             GetComponent<Rigidbody2D>().velocity = Vector2.left * moveSpeed;
         }
         if (Input.GetKey(rightKey))
         {
+            //The sprite is flipped back using the 'flipX' method from the SpriteRenderer so it points the starting direction.
+            RedSpriteRenderer.flipX = false;
             GetComponent<Rigidbody2D>().velocity = Vector2.right * moveSpeed;
         }
 
