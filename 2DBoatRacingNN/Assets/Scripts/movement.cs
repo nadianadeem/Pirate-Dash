6bf59a61654f0d98f0cb5cbc31e43a35afc3d669
nadianadeem <55580386+nadianadeem@users.noreply.github.com>
2019-10-17 19:50:39 +0100

    Added movement script to the project so the player can move

---
+++ 2DBoatRacingNN/Assets/Scripts/movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public KeyCode upKey;
+    public KeyCode downKey;
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+
+    public float moveSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(upKey))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * moveSpeed;
+        }
+        if (Input.GetKey(downKey))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.down * moveSpeed;
+        }
+        if (Input.GetKey(leftKey))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.left * moveSpeed;
+        }
+        if (Input.GetKey(rightKey))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.right * moveSpeed;
+        }
+
+    }
+}
