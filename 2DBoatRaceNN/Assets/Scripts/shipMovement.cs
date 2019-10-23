1041a5a309a833007624c4e425c11f9a38a7be6d
nadianadeem <55580386+nadianadeem@users.noreply.github.com>
2019-10-23 22:49:04 +0100

    started again
    Boat movement working. Shoots bullets

---
+++ 2DBoatRaceNN/Assets/Scripts/shipMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shipMovement : MonoBehaviour
+{
+    public KeyCode upKey;
+    public KeyCode downKey;
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+    private int moveSpeed = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(upKey) && transform.position.x > -4.41 && transform.position.x < 8.21)
+        {
+            transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+
+        }
+
+        else if (Input.GetKey(downKey))
+        {
+            transform.Translate(Vector2.down * moveSpeed * Time.deltaTime);
+        }
+
+        else if (Input.GetKey(rightKey))
+        {
+            transform.Rotate(Vector3.back * 2);
+        }
+
+        else if (Input.GetKey(leftKey))
+        {
+            transform.Rotate(Vector3.forward * 2);
+        }
+
+        
+
+    }
+}
