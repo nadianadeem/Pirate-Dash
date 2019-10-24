904f79269ed4efd5dd8cff80bee608c001f05c62
nadianadeem <55580386+nadianadeem@users.noreply.github.com>
2019-10-24 19:57:15 +0100

    V3
    scroller implemented with a parallaxing background.

---
+++ 2DBoatRaceNN/Assets/Scripts/scroller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scroller : MonoBehaviour
+{
+    Material material;
+    Vector2 offset;
+
+    public float moveX;
+    public float moveY;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector2(moveX, moveY);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        material.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
