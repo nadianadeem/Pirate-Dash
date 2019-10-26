ed3da42380898491b619e74c32663dba840a30ca
nadianadeem <55580386+nadianadeem@users.noreply.github.com>
2019-10-26 10:27:56 +0100

    V6
    Background changed, should be final change for that. 2 logs randomly spawn every 300 frames. Collision work. Sprite sheet added.

---
+++ 2DBoatRaceNN/Assets/Scripts/spawnLog.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnLog : MonoBehaviour
+{
+    public GameObject log;
+    private int time;
+    private Vector3[ , ] spawnPoints = new Vector3[3, 1] { {new Vector3(-2, 6 , 0) }, {new Vector3( 6, 6, 0) },  { new Vector3(0, 6, 0) } };
+    private Vector3[,] spawnPoints2 = new Vector3[2, 1] { { new Vector3(2, 6, 0) }, { new Vector3(-4, 6, 0) } };
+    private int position;
+    private int position2;
+    private  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position = Random.Range(0, 3);
+        position2 = Random.Range(0, 2);
+        time += 1;
+        if (time == 300)
+        {
+            Instantiate(log, spawnPoints[position,0], Quaternion.identity);
+            Instantiate(log, spawnPoints2[position2, 0], Quaternion.identity);
+            time = 0;
+        }
+
+    }
+}
---
+++ 2DBoatRaceNN/Assets/logMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class logMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.down * 6;
+
+        if (transform.position.y < -5.50)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
